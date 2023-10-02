4e0dcd21dfc614c935d1fc7e3b95dc7a87ce7c91
watanaberaiki <k021g1324@g.neec.ac.jp>
2023-10-02 14:54:16 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include<cstdio>
 #include<cstdlib>
 
-int main() {
-	printf("Hello World!!\n");
+int main(int argc,char*argv[]) {
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		//•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
